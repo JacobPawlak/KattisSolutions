187f7e8772650b4b77f7ce3f245e9a40b86d59ce
Jacob Pawlak <jcpa229@cs.uky.edu>
2017-11-12 11:07:18 -0500

    added the solution to quick estimate in c++

---
+++ quickestimates.cpp
@@ -0,0 +1,23 @@
+//Jacob Pawlak
+//November 12th, 2017
+//Quick Estimates
+//https://open.kattis.com/problems/quickestimate
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+
+	int times = 0;
+	cin >> times;
+	for(int i = 0; i < times; i++){
+
+		string estimate;
+		cin >> estimate;
+		cout << estimate.length() << endl;
+
+	}
+
+}
