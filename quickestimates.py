e1fd3713ce3e86d710811b48544d63e84852dac9
Jacob Pawlak <jcpa229@cs.uky.edu>
2017-11-12 11:20:00 -0500

    added the solution to quick estimates in python

---
+++ quickestimates.py
@@ -0,0 +1,13 @@
+#Jacob Pawlak
+#November 12th, 2017
+#Quick Estimates
+#https://open.kattis.com/problems/quickestimate
+
+def main():
+
+	times = int(input())
+	for i in range(0, times):
+		estimate = input()
+		print(len(estimate))
+
+main()
